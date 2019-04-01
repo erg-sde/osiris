e75cc77de48ac8417be30b0467f09d99bab0acae
Ed <ergonzalezpmp@gmail.com>
2019-03-31 21:06:07 -0400

    added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :views
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: redirect('/uc.html')
+  unauthenticated do
+    get '/', to: redirect('/uc.html')
+  end
+ 
+ authenticated do
+  #  root :to => 'dashboard#index'
+ end
 end
