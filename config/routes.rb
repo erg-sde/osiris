7da2d8ad81033d74112c01f2d89732e7786dc574
Ed <ergonzalezpmp@gmail.com>
2019-03-31 20:35:52 -0400

    route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  match '/', :to => redirect('/foo.html')
+  # match '/', :to => redirect('/foo.html')
 end
