f9f545629b81cc5661d7686a3bae7105ba308dcb
Ed <ergonzalezpmp@gmail.com>
2019-04-03 13:15:45 -0400

    added location methods to reflect aggregate capacity information

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,18 @@
 class Location < ApplicationRecord
   has_many :batches
 
+  def self.total_capacity
+    Location.all.sum('capacity')
+  end
+
+  def self.total_allocated_capacity
+    Location.all.sum(&:allocated_capacity)
+  end
+
+  def self.total_available_capacity
+    Location.total_capacity - Location.total_allocated_capacity
+  end
+
   def allocated_capacity
     batches.sum('quantity')
   end
