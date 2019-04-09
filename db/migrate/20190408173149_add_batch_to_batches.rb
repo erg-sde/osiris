7e3451b0f36d94014e54f2710fc9dfe67aca50c1
Ed <ergonzalezpmp@gmail.com>
2019-04-09 16:43:03 -0400

    changed parent to index on batches migration

--- db/migrate/20190408173149_add_batch_to_batches.rb
+++ db/migrate/20190408173149_add_batch_to_batches.rb
@@ -1,5 +1,5 @@
 class AddBatchToBatches < ActiveRecord::Migration[5.2]
   def change
-    add_reference :batches, :parent, foreign_key: true
+    add_reference :batches, :parent, index: true
   end
 end
